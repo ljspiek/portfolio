98f205ec7c6deee2d492c6ecae2f9c49977a8a91
Laura Spieker <ljspiek@gmail.com>
2020-10-07 20:45:28 -0500

    neon flicker test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function App() {
-  return <main className="App">{/* content goes here */}</main>;
+  return (
+    <main className="App">
+      <body>
+        <div class="sign">
+          <span class="fast-flicker">b</span>rea<span class="flicker">t</span>he
+        </div>
+      </body>
+    </main>
+  );
 }
 
 export default App;
